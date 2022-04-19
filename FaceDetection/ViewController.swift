5b67a51471b978d453905d7d29714585fb2880bb
igibliss00@gmail.com <igibliss00@gmail.com>
2022-04-19 18:22:03 -0400

    Create layouts

--- FaceDetection/ViewController.swift
+++ FaceDetection/ViewController.swift
@@ -14,8 +14,11 @@ final class ViewController: UIViewController {
     private var previewLayer: AVCaptureVideoPreviewLayer!
     private var videoDataOutput: AVCaptureVideoDataOutput!
     private var faceLayers: [CAShapeLayer]!
-    private var containerView: UIView!
+    private var cameraContainerView: UIView!
+    private var counterContainrView: UIView!
     private var stackView: UIStackView!
+    private var counterLabel: UILabel!
+    private var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,29 +46,67 @@ final class ViewController: UIViewController {
     }
     
     private func configurePreview() {
-        containerView = UIView()
-        containerView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(containerView)
+        cameraContainerView = UIView()
+        cameraContainerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(cameraContainerView)
         
         previewLayer.videoGravity = .resizeAspectFill
+        cameraContainerView.layer.addSublayer(previewLayer)
+        previewLayer.frame = CGRect(origin: .zero, size: CGSize(width: cameraContainerView.bounds.width, height: cameraContainerView.bounds.height))
         
+        self.videoDataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_32BGRA)] as [String : Any]
+        self.videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "camera"))
+        self.captureSession.addOutput(self.videoDataOutput)
+        
+        let videoConnection = self.videoDataOutput.connection(with: .video)
+        videoConnection?.videoOrientation = .portrait
     }
     
     private func configureCounter() {
-        stackView = UIStackView()
+        counterContainrView = UIView()
+        counterContainrView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(counterContainrView)
+        
+        counterLabel = UILabel()
+        counterLabel.text = "0"
+        counterLabel.font = UIFont.systemFont(ofSize: 50, weight: .bold)
+        counterLabel.adjustsFontForContentSizeCategory = true
+        counterLabel.accessibilityHint = "Number of faces detected"
+        counterLabel.textAlignment = .center
+        
+        let imageConfig = UIImage.SymbolConfiguration(scale: .medium)
+        let image = UIImage(systemName: "person.2.fill", withConfiguration: imageConfig)?.withTintColor(.gray, renderingMode: .alwaysOriginal)
+        imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        imageView.accessibilityHint = "Camera to detect faces"
+        
+        stackView = UIStackView(arrangedSubviews: [counterLabel, imageView])
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
         stackView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(stackView)
     }
 
     private func setConstraints() {
         NSLayoutConstraint.activate([
-            containerView.topAnchor.constraint(equalTo: view.topAnchor),
-            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            containerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.6),
+            cameraContainerView.topAnchor.constraint(equalTo: view.topAnchor),
+            cameraContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            cameraContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            cameraContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.6),
             
-            stackView.topAnchor.constraint(equalTo: containerView.bottomAnchor)
+            counterContainrView.topAnchor.constraint(equalTo: cameraContainerView.bottomAnchor),
+            counterContainrView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            counterContainrView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            counterContainrView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            stackView.heightAnchor.constraint(equalTo: counterContainrView.heightAnchor, multiplier: 0.6),
+            stackView.widthAnchor.constraint(equalToConstant: 100),
+            stackView.centerXAnchor.constraint(equalTo: counterContainrView.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: counterContainrView.centerYAnchor)
         ])
     }
 }
 
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+
+}
